5ea9506e9e95706480dd8d5e1175ac0d994cefe7
Flores Eken <flores@eken.nl>
2018-03-27 09:41:45 +0200

    Fixing method docu

--- BlazorExtensions/Http.cs
+++ BlazorExtensions/Http.cs
@@ -10,18 +10,16 @@ namespace BlazorExtensions.Http
     /// </summary>
     public static class BlazorHttpExtensions
     {
-
-
-
         /// <summary>
         /// Sends an HTTP Post request to the specified URI, including the specified <paramref name="content"/>
         /// in JSON-encoded format, and doed NOT parse the response body, just returns a string.
+        /// Useful for requesting tokens
         /// </summary>
         /// <param name="httpClient">The <see cref="HttpClient"/>.</param>
         /// <param name="method">The HTTP method.</param>
         /// <param name="requestUri">The URI that the request will be sent to.</param>
         /// <param name="content">Content for the request body. This will be JSON-encoded and sent as a string.</param>
-        /// <returns>The response parsed as an object of the generic type.</returns>
+        /// <returns>The response as a string (unparsed)</returns>
         public static async Task<string> SendJsonAsyncRawResult(this HttpClient httpClient, HttpMethod httpMethod, string requestUri, object content)
         {
             var requestJson = JsonUtil.Serialize(content);
